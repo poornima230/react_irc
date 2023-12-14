8dc27eee19c169d70b30f2e185a10533e5a009aa
poornima230 <153356656+poornima230@users.noreply.github.com>
2023-12-14 10:00:54 +0530

    Add files via upload

---
+++ cc project/SignIn.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+function SignInForm() {
+  const [state, setState] = React.useState({
+    email: "",
+    password: ""
+  });
+  const handleChange = evt => {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value
+    });
+  };
+
+  const handleOnSubmit = evt => {
+    evt.preventDefault();
+
+    const { email, password } = state;
+    if (email=== 'user@gmail.com' && password === 'password') {
+        // Successful login
+        alert('Login Successful!');
+      } else {
+        alert('Invalid username or password');
+      }
+   // alert(`Please enter your email ${email}and password ${password} to login in our website`);
+
+    for (const key in state) {
+      setState({
+        ...state,
+        [key]: ""
+      });
+    }
+  };
+
+  return (
+    <div className="form-container sign-in-container">
+      <form onSubmit={handleOnSubmit}>
+        <h1>Sign in</h1>
+       
+        
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={state.password}
+          onChange={handleChange}
+        />
+        <a href="#">Forgot your password?</a>
+        <button>Sign In</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignInForm;
